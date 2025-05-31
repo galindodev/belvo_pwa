9d6b06e115f6b25f97936ea6fa5dc6d1ead548de
JhormanGalindo <j.galindop@uniandes.edu.co>
2025-05-30 22:13:12 -0500

    fix: update base URL to production endpoint

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,2 +1,2 @@
-// const String baseUrl = "https://api.galindodev.com";
-const String baseUrl = "http://127.0.0.1:5000";
+const String baseUrl = "https://api.galindodev.com";
+// const String baseUrl = "http://127.0.0.1:5000";
